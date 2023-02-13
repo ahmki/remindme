7a43967e734a917dd55f0f020c48f45fb140d676
ahmki <aleksi.heinimaki1@gmail.com>
2023-02-13 19:35:56 +0200

    Initial backend functionality

--- project.clj
+++ project.clj
@@ -3,6 +3,11 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.0"]]
+  :dependencies [
+                 [org.clojure/clojure "1.10.0"]
+                 [ring/ring-jetty-adapter "1.9.6"]
+                 [ring/ring-defaults "0.3.4"]
+                 [metosin/reitit "0.5.18"]
+                 [hiccup "2.0.0-alpha2"]]
   :repl-options {:init-ns remindme.core}
   :main remindme.core)
--- src/remindme/core.clj
+++ src/remindme/core.clj
@@ -1,5 +1,15 @@
-(ns remindme.core)
+(ns remindme.core
+  (:gen-class)
+  (:require [ring.adapter.jetty :as jetty]))
 
-(defn -main
-  []
-  (println "Hello, World!"))
+(def server (atom nil))
+
+(defn handler [req]
+  {:status 200
+   :body "OK2"})
+
+(defn -main [& args]
+  (swap! server
+         (fn [_]
+           (jetty/run-jetty #'handler {:join? false
+                                       :port 3428}))))
