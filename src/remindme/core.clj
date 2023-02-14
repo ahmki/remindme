284a6e9fd1ea7fe8d6edfc0a95620de68e87cb92
ahmki <aleksi.heinimaki1@gmail.com>
2023-02-14 19:32:18 +0200

    Add functionality

--- src/remindme/core.clj
+++ src/remindme/core.clj
@@ -1,15 +1,29 @@
 (ns remindme.core
   (:gen-class)
-  (:require [ring.adapter.jetty :as jetty]))
+  (:require [ring.adapter.jetty :as jetty]
+            [ring.middleware.defaults :as ring-defaults]))
 
-(def server (atom nil))
+(defonce server (atom nil))
 
 (defn handler [req]
+  (prn req)
   {:status 200
    :body "OK2"})
 
+(defn start-jetty! [& _]
+  (reset!
+    server
+    (jetty/run-jetty (ring-defaults/wrap-defaults
+                       #'handler
+                       ring-defaults/site-defaults)
+                     {:join? false
+                      :port 3428})))
+
+(defn stop-jetty! []
+  (.stop @server)
+  (reset! server nil))
+
 (defn -main [& args]
-  (swap! server
-         (fn [_]
-           (jetty/run-jetty #'handler {:join? false
-                                       :port 3428}))))
+  (start-jetty!))
+
+(stop-jetty!)
